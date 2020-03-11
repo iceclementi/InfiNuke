7ad6335530df806cf0de7c70f5534578b5871eb6
iceclementi <iceclementi@gmail.com>
2020-03-12 02:29:59 +0800

    Initialise GUI to display logo and welcome message on startup

--- src/main/java/seedu/nuke/gui/Main.java
+++ src/main/java/seedu/nuke/gui/Main.java
@@ -2,33 +2,44 @@ package seedu.nuke.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import seedu.nuke.command.CommandResult;
+import seedu.nuke.data.ModuleManager;
+import seedu.nuke.ui.Ui;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private CommandResult commandResult;
+    private ModuleManager moduleManager;
+    private Ui ui;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
+    public void init() throws Exception {
+        this.moduleManager = new ModuleManager();
+        this.ui = new Ui();
+        // Load modules and tasks
+    }
+
+    @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("V \u2022 NUS");
-        // stage.setMaximized(true);
         stage.getIcons().add(new Image("seedu/nuke/gui/resource/image/venus_icon.png"));
-
-        Parent homeRoot = FXMLLoader.load(getClass().getResource("resource/main.fxml"));
-        Scene home = new Scene(homeRoot);
-
-        home.setCursor(Cursor.DEFAULT);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
 
 
-        stage.setScene(home);
+        Parent mainRoot = FXMLLoader.load(getClass().getResource("resource/main.fxml"));
+        Scene main = new Scene(mainRoot);
+
+        stage.setScene(main);
         stage.show();
     }
 }
--- src/main/java/seedu/nuke/gui/controller/MainController.java
+++ src/main/java/seedu/nuke/gui/controller/MainController.java
@@ -1,15 +1,46 @@
 package seedu.nuke.gui.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static seedu.nuke.util.Message.*;
+
+public class MainController implements Initializable {
+    @FXML
+    private TextFlow consoleScreen;
+
     @FXML
     private TextField console;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        consoleScreen.setStyle("-fx-font-family: Consolas; -fx-font-size: 12pt");
+        Text logo = createText(MESSAGE_LOGO, Color.MAGENTA);
+        Text welcomeMessage = createText(String.format("%s\n%s\n", MESSAGE_WELCOME_1, MESSAGE_WELCOME_2), Color.BLUE);
+        Text divider = createText("-".repeat(80) + "\n");
+
+        consoleScreen.getChildren().addAll(logo, divider, welcomeMessage);
+    }
 
     public void submitInput() {
         System.out.println(console.getText());
         console.clear();
+    }
+
+    private Text createText(String input) {
+        return new Text(input);
+    }
+
+    private Text createText(String input, Color color) {
+        Text text = new Text(input);
+        text.setFill(color);
+        return text;
     }
 }
